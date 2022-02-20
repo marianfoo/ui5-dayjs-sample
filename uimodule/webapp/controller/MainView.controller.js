c1fc0688d4b83fa9456d6e0d1db85e683becb106
marianfoo <13335743+marianfoo@users.noreply.github.com>
2022-02-20 17:52:25 +0100

    init

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,13 +1,27 @@
 sap.ui.define(
-    ["./BaseController"],
+    ["./BaseController",
+    "sap/ui/model/json/JSONModel"],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
+     * @param {typeof sap.ui.model.json.JSONModel} JSONModel 
+ */
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("de.marianzeis.datelibsample.controller.MainView", {
-            onInit: function () {},
+            _data: {
+                "date": "2014/12/30",
+                "inputFormat": "yyyy/MM/dd",
+                "outputFormat": "dd.MM.YYYY"
+            },
+
+            onInit: function (evt) {
+                var oModel = new JSONModel(this._data);
+                this.getView().setModel(oModel);
+            },
+            onPress: function (evt) {
+                console.log("")
+            }
         });
     }
 );
