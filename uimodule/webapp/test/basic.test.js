ef21945991da69cefa8a8fe2d0c66e5ab469ba0c
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2022-03-06 12:28:40 +0100

    test with language

--- uimodule/webapp/test/basic.test.js
+++ uimodule/webapp/test/basic.test.js
@@ -1,8 +1,10 @@
-const Main = require("./pageObjects/Main");
+const Main = require("./pageObjects/MainDE");
+const MainDE = require("./pageObjects/MainDE");
 
 describe("ui5 basic", () => {
     before(async () => {
         await Main.open();
+        // await MainDE.open();
     });
 
     // it("should have the right title", async () => {
@@ -10,9 +12,14 @@ describe("ui5 basic", () => {
     //     expect(title).toEqual("appTitle")
     // })
 
-    it("get field", async () => {
+    it("get field english", async () => {
         const field = await Main.getField("testId");
         const text = await field.getText();
-        expect(text).toEqual("01.01.2020");
+        expect(text).toEqual("Tu.12.2014");
     });
+    // it("get field german", async () => {
+    //     const field = await Main.getField("testId");
+    //     const text = await field.getText();
+    //     expect(text).toEqual("Di.12.2014");
+    // });
 });
---
+++ uimodule/webapp/test/pageObjects/MainDE.js
@@ -0,0 +1,24 @@
+const Page = require("./Page")
+
+class Main extends Page {
+    async open() {
+        // await super.open(`?sap-language=de`)
+        await super.open(`#demarianzeisdatelibsample-tile`)
+    }
+
+    _viewName = "de.marianzeis.datelibsample.view.MainView"
+
+
+    async getField(id){
+        const cbSelector1 = {
+            selector: {
+                id: id,
+                viewName: this._viewName
+            }
+        }
+
+        return await browser.asControl(cbSelector1)
+    }
+}
+
+module.exports = new Main()
